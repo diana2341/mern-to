d5e31c364df276ae45c5411aa85e286013210672
diana2341 <dianaponce2341@gmail.com>
2021-02-23 19:57:36 -0500

    bootstrap

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <div className="container">
+    <h2>MERN-Stack Todo App</h2>
+  </div>
   );
 }
 
